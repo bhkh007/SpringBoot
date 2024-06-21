6dbc4e8828864d913cc662788fdc2af60281ebf8
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-21 21:46:44 +0530

    completed this project by adding one more feature called localization by adding french german and us language and passing them in messages

--- ProductRest/src/main/java/com/example/demo/controller/ProductController.java
+++ ProductRest/src/main/java/com/example/demo/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,13 +30,25 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@GetMapping("/greet/{name}")
+	public ResponseEntity<?> greet(@PathVariable("name") String name, @RequestHeader(name="Accept-Lanuage",required=false)Locale locale){
+		String messages = messageSource.getMessage("greeting.message", new Object[] {"name"}, locale);
+		HashMap<String, String> responseBody = new HashMap<>();
+		responseBody.put(ResponseKey.MESSAGE, messages);
+		
+		return new ResponseEntity<>(responseBody,HttpStatus.OK);
+	}
+	
 	@GetMapping("/")
-	public ResponseEntity<?> findAll() {
+	public ResponseEntity<?> findAll(@RequestHeader(name="Accept-Lanuage",required=false)Locale locale) {
 		try {
 			List<Product> productsList = productService.findAll();
 			if(productsList.isEmpty()) {
 				HashMap< String, String> data = new HashMap<>();
-				data.put(ResponseKey.MESSAGE, ResponseMessage.NO_PRODUCT_FOUND);
+				data.put(ResponseKey.MESSAGE, messageSource.getMessage("error.noproductavailable", null,locale));
 				return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
 			}else {
 				HashMap<String, Object> data = new HashMap<>();
@@ -44,14 +58,14 @@ public class ProductController {
 			}
 		} catch (Exception e) {
 			HashMap< String, String> data = new HashMap<>();
-			data.put(ResponseKey.MESSAGE,ResponseMessage.SOMETHING_WENT_WRONG);
+			data.put(ResponseKey.MESSAGE,messageSource.getMessage("error.exception", null, locale));
 			return new ResponseEntity<>(data,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<?> getById(@PathVariable int id){
+	public ResponseEntity<?> getById(@PathVariable int id,@RequestHeader(name="Accept-Lanuage",required=false)Locale locale){
 		HashMap< Object, Object> data = new HashMap<>();
 		try {
 			Optional<Product> productOptional = productService.getbyId(id);
@@ -60,30 +74,30 @@ public class ProductController {
 				return new ResponseEntity<>(product,HttpStatus.OK);
 			}
 			else {
-				data.put(ResponseKey.MESSAGE, ResponseMessage.NO_PRODUCT_FOUND_BY_ID);
+				data.put(ResponseKey.MESSAGE,messageSource.getMessage("error.productnotfound", null, locale));
 				return new ResponseEntity<>(data,HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			data.put(ResponseKey.MESSAGE,ResponseMessage.SOMETHING_WENT_WRONG);
+			data.put(ResponseKey.MESSAGE,messageSource.getMessage("error.exception", null, locale));
 			return new ResponseEntity<>(data,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
 	@PostMapping("/")
-	public ResponseEntity<?> save(@RequestBody Product product) {
+	public ResponseEntity<?> save(@RequestBody Product product,@RequestHeader(name="Accept-Lanuage",required=false)Locale locale) {
 		try {
 			Product savedProduct = productService.save(product);
 			return new ResponseEntity<>(savedProduct,HttpStatus.CREATED);
 		} catch (Exception e) {
 			HashMap< String, String> data = new HashMap<>();
-			data.put(ResponseKey.MESSAGE,ResponseMessage.SOMETHING_WENT_WRONG);
+			data.put(ResponseKey.MESSAGE,messageSource.getMessage("error.exception", null, locale));
 			return new ResponseEntity<>(data,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> remove(@PathVariable int id){
+	public ResponseEntity<?> remove(@PathVariable int id,@RequestHeader(name="Accept-Lanuage",required=false)Locale locale){
 		HashMap< Object, Object> data = new HashMap<>();
 		try {
 			Optional<Product> productOptional = productService.getbyId(id);
@@ -92,17 +106,17 @@ public class ProductController {
 				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 			}
 			else {
-				data.put(ResponseKey.MESSAGE, ResponseMessage.NO_PRODUCT_FOUND_BY_ID);
+				data.put(ResponseKey.MESSAGE, messageSource.getMessage("error.productnotfound", null, locale));
 				return new ResponseEntity<>(data,HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			data.put(ResponseKey.MESSAGE,ResponseMessage.SOMETHING_WENT_WRONG);
+			data.put(ResponseKey.MESSAGE,messageSource.getMessage("error.exception", null, locale));
 			return new ResponseEntity<>(data,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
 	@GetMapping("/byName/{name}")
-	public ResponseEntity<?> findByName(@PathVariable String name){
+	public ResponseEntity<?> findByName(@PathVariable String name,@RequestHeader(name="Accept-Lanuage",required=false)Locale locale){
 		HashMap< Object, Object> data = new HashMap<>();
 		try {
 			
@@ -110,14 +124,14 @@ public class ProductController {
 			return new ResponseEntity<>(productList,HttpStatus.OK);
 			
 		} catch (Exception e) {
-			data.put(ResponseKey.MESSAGE,ResponseMessage.SOMETHING_WENT_WRONG);
+			data.put(ResponseKey.MESSAGE,messageSource.getMessage("error.exception", null, locale));
 			return new ResponseEntity<>(data,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 	}
 	
 	@GetMapping("/{id}/reviews")
-	public ResponseEntity<?> findAllReviewsByProduct(@PathVariable int id){
+	public ResponseEntity<?> findAllReviewsByProduct(@PathVariable int id,@RequestHeader(name="Accept-Lanuage",required=false)Locale locale){
 		HashMap< Object, Object> data = new HashMap<>();
 		try {
 			Optional<Product> productOptional = productService.getbyId(id);
@@ -127,12 +141,12 @@ public class ProductController {
 				return new ResponseEntity<>(reviews ,HttpStatus.OK);
 			}
 			else {
-				data.put(ResponseKey.MESSAGE, ResponseMessage.NO_PRODUCT_FOUND_BY_ID);
+				data.put(ResponseKey.MESSAGE, messageSource.getMessage("error.productnotfound", null, locale));
 				return new ResponseEntity<>(data,HttpStatus.OK);
 			}
 			
 		} catch (Exception e) {
-			data.put(ResponseKey.MESSAGE,ResponseMessage.SOMETHING_WENT_WRONG);
+			data.put(ResponseKey.MESSAGE,messageSource.getMessage("error.exception", null, locale));
 			return new ResponseEntity<>(data,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
